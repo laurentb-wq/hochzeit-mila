e06d6803948ffe0dce4e39f5b096407260502731
Laurent Born <LaurentBorn@Laurents-MBP.sunrise.box>
2026-04-17 23:44:59 +0200

    fix build errors

--- app/components/BokehBackground.tsx
+++ app/components/BokehBackground.tsx
@@ -46,7 +46,7 @@ export default function BokehBackground() {
   useEffect(() => {
     const canvas = canvasRef.current!;
     if (!canvas) return;
-    const ctx = canvas.getContext("2d");
+    const ctx = canvas.getContext("2d")!;
     if (!ctx) return;
 
     let animId: number;
